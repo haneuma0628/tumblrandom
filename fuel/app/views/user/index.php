b4da3283e4b9bd36cfcf0b50b3e14d37d834c43a
hnmxxxx <haneuma0628@gmail.com>
2015-05-13 06:57:15 +0900

    fuelPHP版を破棄

--- fuel/app/views/user/index.php
+++
@@ -1,7 +0,0 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "index" ); ?>'><?php echo Html::anchor('user/index','Index');?></li>
-	<li class='<?php echo Arr::get($subnav, "login" ); ?>'><?php echo Html::anchor('user/login','Login');?></li>
-	<li class='<?php echo Arr::get($subnav, "posts" ); ?>'><?php echo Html::anchor('user/posts','Posts');?></li>
-
-</ul>
-<p>Index</p>
