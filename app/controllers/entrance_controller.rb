babc742fb77cd03876c9a2124e3ca39f586774ae
haneuma0628 <haneuma0628@gmail.com>
2015-06-13 02:17:15 -0700

    ログイン済みでルートにアクセスするとdashbordに飛ぶようにした

--- app/controllers/entrance_controller.rb
+++ app/controllers/entrance_controller.rb
@@ -2,5 +2,8 @@ class EntranceController < ApplicationController
   layout "entrance"
 
   def index
+    if user_signed_in? then
+      redirect_to '/users/dashbord'
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,9 @@ require 'tumblr_client'
 class UsersController < ApplicationController
   layout "application"
 
-  def dashbord
-    Tumblr.configure do |config|
-      config.consumer_key = ENV['TUMBLRANDOM_CONSUMER_KEY']
-      config.consumer_secret = ENV['TUMBLRANDOM_CONSUMER_SECRET']
-      config.oauth_token = current_user.access_token
-      config.oauth_token_secret = current_user.access_secret
-    end
+  before_action :authenticate_tumblr_client, :authenticate_user!
 
+  def dashbord
     client = Tumblr::Client.new
     @tum = client.info
   end
@@ -29,4 +24,15 @@ class UsersController < ApplicationController
 
   def select_action_blog
   end
+
+  private
+  def authenticate_tumblr_client
+    Tumblr.configure do |config|
+      config.consumer_key = ENV['TUMBLRANDOM_CONSUMER_KEY']
+      config.consumer_secret = ENV['TUMBLRANDOM_CONSUMER_SECRET']
+      config.oauth_token = current_user.access_token
+      config.oauth_token_secret = current_user.access_secret
+    end
+  end
+
 end
